eda4d76c87a7a06845c8618064dd8d0f18fd9c17
sarmiladangol <sarmila_01323@hotmail.com>
2016-08-03 13:32:12 -0400

    checkNullValuesforPlaces is now working

--- FoodNetworkListTableViewController.m
+++ FoodNetworkListTableViewController.m
@@ -52,6 +52,7 @@
 }
 
 -(void)getRestaurantFromDatabase{
+    NSLog(@"GET RESTAURANT FROM DATABASE");
     _restaurantArray = [[NSMutableArray alloc]init];
     FIRDatabaseReference *ref = [[FIRDatabase database]reference];
     FIRDatabaseReference *restaurantsRef = [ref child:@"restaurants"];
@@ -103,6 +104,7 @@
 
 
 -(void)getCurrentInfo{
+     NSLog(@"GET CURRENT INFO");
     _placesClient = [GMSPlacesClient sharedClient];
     [_placesClient currentPlaceWithCallback:^(GMSPlaceLikelihoodList *placeLikelihoodList, NSError *error){
         
@@ -128,6 +130,7 @@
     }];
 }
 - (IBAction)pickPlace:(id)sender {
+     NSLog(@"PICK PLACE");
     
     CLLocationCoordinate2D center = CLLocationCoordinate2DMake(_locationManager.location.coordinate.latitude, _locationManager.location.coordinate.longitude);
     CLLocationCoordinate2D northEast = CLLocationCoordinate2DMake(center.latitude + 0.001,
@@ -148,28 +151,82 @@
         if (place != nil) {
             NSLog(@"PLACE DESCRIPTION=%@", place.description);
             Restaurant *newRestaurant = [[Restaurant alloc]init];
+            
             newRestaurant.location = place.coordinate;
-            newRestaurant.restaurantName = place.name;
-            newRestaurant.restaurantAddress = [[place.formattedAddress componentsSeparatedByString:@", "] componentsJoinedByString:@"\n"];
-            newRestaurant.restaurantPhoneNumber = place.phoneNumber;
+            // newRestaurant.restaurantName = place.name;
+            
+            
+            // check if any PLACE key value is NULL
+            if (place.name == nil) {
+                newRestaurant.restaurantName = @"Name is not available";
+            }
+            else {
+                newRestaurant.restaurantName = place.name;
+            }
+            
+            if (place.phoneNumber == nil) {
+                newRestaurant.restaurantPhoneNumber = @"Phone is not available";
+            }
+            else {
+                newRestaurant.restaurantPhoneNumber = place.phoneNumber;
+            }
+            
+            if (place.website == nil)
+            {
+                newRestaurant.restaurantWebsite = @"Website is not available";
+            }
+            else {
+                //convert WEBSITE from NSURL to NSString in order to store in NSDICTIONARY
+                urlToString = [place.website absoluteString];
+                newRestaurant.restaurantWebsite = urlToString;
+            }
+            
+            if (place.types == nil) {
+                newRestaurant.restaurantTypes = @"Type is not available";
+            }
+            else{
             newRestaurant.restaurantTypes = place.types[1].description;
+            }
             
-            //convert RATING from float to NSString in order to store in NSDICTIONARY
+            //newRestaurant.restaurantPhoneNumber = place.phoneNumber;
+           // newRestaurant.restaurantTypes = place.types[1].description;
             NSString *newRating = [NSString stringWithFormat:@"%f", place.rating];
+            if (newRating == nil) {
+                newRestaurant.restaurantRating = @"Rating is not available";
+                NSLog(@" NIL *** newRestaurant.restaurantRating %@", newRestaurant.restaurantRating);
+            }
+            else{
+            //convert RATING from float to NSString in order to store in NSDICTIONARY
+           // NSString *newRating = [NSString stringWithFormat:@"%f", place.rating];
             newRestaurant.restaurantRating = newRating;
+                NSLog(@" NOT NIL *** newRestaurant.restaurantRating %@", newRestaurant.restaurantRating);
+            }
             
-            //convert WEBSITE from NSURL to NSString in order to store in NSDICTIONARY
-            urlToString = [place.website absoluteString];
-            newRestaurant.restaurantWebsite = urlToString;
+           // newRestaurant.restaurantAddress = [[place.formattedAddress componentsSeparatedByString:@", "] componentsJoinedByString:@"\n"];
             
+
             NSString *formattedAddress = [[place.formattedAddress componentsSeparatedByString:@", "] componentsJoinedByString:@", "];
             
+            if (formattedAddress == nil) {
+                newRestaurant.restaurantAddress = @"Address is not available";
+                NSLog(@"NIL *** newRestaurant.restaurantAddress= %@",newRestaurant.restaurantAddress);
+            }
+            
+            else{
+                newRestaurant.restaurantAddress = formattedAddress;
+                NSLog(@"NOT NIL *** newRestaurant.restaurantAddress= %@",newRestaurant.restaurantAddress);
+            }
+            
             NSString *locationStringToPass = [NSString stringWithFormat:@"%f, %f", newRestaurant.location.latitude, newRestaurant.location.longitude];
+            
+           
             
             newRestaurant.restaurantLatitude = [NSString stringWithFormat:@"%f", newRestaurant.location.latitude];
             newRestaurant.restaurantLongitude = [NSString stringWithFormat:@"%f", newRestaurant.location.longitude];
             
-            NSDictionary *newRestaurantInfo = @{@"restaurant_name":newRestaurant.restaurantName, @"location":locationStringToPass, @"restaurant_phone":newRestaurant.restaurantPhoneNumber, @"restaurant_address":formattedAddress, @"restaurant_website":newRestaurant.restaurantWebsite,@"restaurant_rating":newRestaurant.restaurantRating, @"restaurant_types":newRestaurant.restaurantTypes};
+            
+            NSDictionary *newRestaurantInfo = @{@"restaurant_name":newRestaurant.restaurantName, @"location":locationStringToPass, @"restaurant_phone":newRestaurant.restaurantPhoneNumber, @"restaurant_address":newRestaurant.restaurantAddress, @"restaurant_website":newRestaurant.restaurantWebsite,@"restaurant_rating":newRestaurant.restaurantRating, @"restaurant_types":newRestaurant.restaurantTypes};
+             NSLog(@"NEW RESTAURANT DICTIONARY = %@",newRestaurant.description);
             
             FIRDatabaseReference *ref = [[FIRDatabase database] reference];
             FIRDatabaseReference *restaurantRef = [ref child:@"restaurants"].childByAutoId;
